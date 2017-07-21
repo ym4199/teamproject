1875afe94530e8aaaba3e442a49dcb740f96dc3d
Gihong Kim <gyong1211@gmail.com>
2017-07-21 18:06:07 +0900

    set eb deploy environment

--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -2,7 +2,7 @@ from .base import *
 
 config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
 
 DATABASES = config_secret_deploy['django']['database']
